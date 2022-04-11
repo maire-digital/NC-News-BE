3420924738aed95a353031f9bb75227923c72410
Maire <Maire@Maires-MacBook-Air.local>
2022-04-11 14:15:27 -0700

    delete test sql

--- sqltest.sql
+++
@@ -1,6 +0,0 @@
-\c nc_news_test
-
-
-
-SELECT articles.*, COUNT(comments.votes)::int AS comment_count FROM articles LEFT JOIN comments ON articles.article_id = comments.article_id WHERE articles.topic = 'mitch'  GROUP BY articles.article_id ORDER BY votes asc;
-
